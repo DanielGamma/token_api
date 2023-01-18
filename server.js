ee3745c00aea3f9e89c81f0fbbce96e123b7adb4
DanielGamma <daniel.carrasco@gammatech.school>
2023-01-18 21:22:55 +0100

    Add cors

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app = express();
 const port = 3000;
 
 const bcrypt = require("bcrypt");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", async (req, res) => {
@@ -12,24 +14,23 @@ app.get("/", async (req, res) => {
     const { name } = req.body;
     try {
         if (!bearer) {
-            return res.status(401).send({status: "unauthenticated", error: "No token provided." })
-        } 
+            return res.status(401).send({ status: "unauthenticated", error: "No token provided. You can get a token in /auth." })
+        }
         else if (!bearer.startsWith("Bearer ")) {
-            return res.status(401).send({status: "unauthenticated", error: "Token format is not correct. Please, check your syntax." })
+            return res.status(401).send({ status: "unauthenticated", error: "Token format is not correct. Please, check your syntax." })
         }
         else {
             const token = bearer.split(" ")[1];
             const isAuthenticated = await bcrypt.compare(name, token);
             if (!isAuthenticated) {
-                return res.status(401).send({status: "unauthenticated", error: "Did you think you could fool me?! You shall not pass!"})
+                return res.status(401).send({ status: "unauthenticated", error: "Did you think you could fool me?! You shall not pass!" })
             } else {
                 return res.send({ status: "authenticated", message: `Congratulations ${name}!` })
             }
         }
     } catch (error) {
-
+        return res.send({ error: "An error occurred. Contact your handsome teacher for assistance." })
     }
-    res.send("Hello World!");
 });
 
 app.post("/auth", async (req, res) => {
