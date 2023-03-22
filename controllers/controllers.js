a92364c5666aa9b563033f6e344c4fa113ef83a2
DanielGamma <daniel.carrasco@gammatech.school>
2023-03-22 10:38:05 +0100

    Add email and password authentication

---
+++ controllers/controllers.js
@@ -0,0 +1,49 @@
+const bcrypt = require("bcryptjs");
+
+const getAuth = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        if (!email || !password) {
+            return res.status(400).send({ code: 400, message: "No email or password provided. Please, send your credentials to get a token." })
+        }
+        else if (email === "gammatech@gmail.com" && password === "gamma23") {
+            const salt = bcrypt.genSaltSync(5);
+            const hash = bcrypt.hashSync("GammaTechSchool", salt);
+            return res.status(200).send({ code: 200, bearer_token: hash })
+        }
+        else {
+            return res.status(400).send({ code: 400, message: "Wrong email or password. Impersonating a worker of GammaTech School is a crime... Watch out or you'll end up in jail!" })
+        }
+
+    } catch (error) {
+        return res.status(500).send({ code: 500, message: "An error occurred. Contact your handsome teacher for assistance.", error: error })
+    }
+}
+
+const checkAuth = (req, res) => {
+    const bearer = req.headers.authorization;
+    try {
+        if (!bearer) {
+            return res.status(401).send({ code: 401, message: "No token provided. You can get a token in /auth." })
+        }
+        else if (!bearer.startsWith("Bearer ")) {
+            return res.status(401).send({ code: 401, message: "Token format is not correct. Please, check your syntax." })
+        }
+        else {
+            const token = bearer.split(" ")[1];
+            const isAuthenticated = bcrypt.compareSync("GammaTechSchool", token);
+            if (!isAuthenticated) {
+                return res.status(401).send({ code: 401, message: "Did you think you could fool me?! You shall not pass!" })
+            } else {
+                return res.status(200).send({ code: 200, message: `Oh, I konw you, you can pass!` })
+            }
+        }
+    } catch (error) {
+        return res.status(500).send({ error: "An error occurred. Contact your handsome teacher for assistance." })
+    }
+}
+
+module.exports = {
+    getAuth,
+    checkAuth
+}
--- server.js
+++ server.js
@@ -3,57 +3,17 @@ const app = express();
 
 const port = 3000;
 
-//const bcrypt = require("bcrypt");
 const bcryptjs = require("bcryptjs");
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-    const bearer = req.headers.authorization;
-    const { name } = req.query;
-    try {
-        if (!bearer) {
-            return res.status(401).send({ status: "unauthenticated", error: "No token provided. You can get a token in /auth." })
-        }
-        else if (!bearer.startsWith("Bearer ")) {
-            return res.status(401).send({ status: "unauthenticated", error: "Token format is not correct. Please, check your syntax." })
-        }
-        else {
-            const token = bearer.split(" ")[1];
-            // Change bcrypt to bcryptjs
-            const isAuthenticated = bcryptjs.compareSync(name, token);
-            //const isAuthenticated = await bcrypt.compare(name, token);
-            if (!isAuthenticated) {
-                return res.status(401).send({ status: "unauthenticated", error: "Did you think you could fool me?! You shall not pass!" })
-            } else {
-                return res.send({ status: "authenticated", message: `Congratulations ${name}!` })
-            }
-        }
-    } catch (error) {
-        return res.send({ error: "An error occurred. Contact your handsome teacher for assistance." })
-    }
-});
+const { getAuth, checkAuth } = require("./controllers/controllers.js");
 
-app.post("/auth", async (req, res) => {
-    const { name } = req.body;
-    try {
-        if (name) {
-            // Change bcrypt to bcryptjs
-            const salt = bcryptjs.genSaltSync(5);
-            const hash = bcryptjs.hashSync(name, salt);
-            //const hash = await bcrypt.hash(name, 5);
-            res.send({ token: hash });
-        } else {
-            res.send({ error: "No name provided. Please, send your name to get a token." })
-        }
+app.get("/", checkAuth);
 
-    } catch (error) {
-        res.send({ error: "An error occurred. Contact your handsome teacher for assistance." })
-    }
-
-});
+app.post("/auth", getAuth);
 
 app.listen(port, () => {
     console.log(`App listening in port ${port}`);
